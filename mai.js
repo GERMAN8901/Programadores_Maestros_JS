4682702fb6c77acf495ed45a304f74d1e8518622
GERMAN8901 <106921625+GERMAN8901@users.noreply.github.com>
2022-10-09 22:10:48 -0500

    se crea un arreglo para los trabajadores

--- mai.js
+++ mai.js
@@ -1,3 +1,14 @@
-alert("Mensaje de alerta");
+//alert("Mensaje de alerta");
 
-var nombre = "empresa" ;
+let nombre = "razon_social";
+const nit = "nit";
+let direccion = "direccion";
+const telefono = "telefono";
+let Region = "Region";
+
+
+console.log(nombre + " " + nit + " " 
++ "direccion" + " " + telefono + " " + Region);
+
+let trabajadores = ["maria","pedro"];   // arreglos forma 1
+trabajadores2[2]="german";              // arreglos forma 2
